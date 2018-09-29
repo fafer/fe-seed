b5f9055ac2fab6217b13cae9898d4e3e2c57298a
fafer <zhoucheng502@sina.com>
2018-09-29 19:41:55 +0800

    update

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -8,17 +8,33 @@ module.exports = Merge(CommonConfig, {
     mode:'development',
     devtool: 'inline-source-map',
     devServer: {
-        host: '0.0.0.0',
+        host: '0.0.0.0',            //允许外网访问，默认值为localhost
         port: 8041,
-        open:true,
-        disableHostCheck: true,
-        allowedHosts:[],
-        hot: true,
-        contentBase: [conf.ENTRY_PATH,path.join(conf.ROOT_PATH)],
-        compress: true,
-        publicPath: conf.PUBLICBASE ? conf.PUBLICBASE + '/':'/',
-        // historyApiFallback: true,
-        proxy: [
+        open:true,                  //服务启动后，打开浏览器默认访问：http://host:port/
+        openPage:'',                //设置默认打开的页面路径
+        public: "localhost",        //配置服务启动后，打开浏览器访问http://host:port/失败后，转发到此//host:port
+        // useLocalIp: true,           //配置服务启动后，打开浏览器访问:http://本地ip:port/,如果失败，转发到public设置
+        // https: true,             //开启https
+        disableHostCheck: false,     //允许所有host域名访问
+        allowedHosts:[],            //设置允许访问的域名白名单
+        hot: true,                  //开启热更新
+        hotOnly: true,              //编译失败后，再次编译成功，不需要刷新页面更新
+        contentBase: [conf.ENTRY_PATH,path.join(conf.ROOT_PATH)], //配置访问静态资源根目录
+        compress: true,             //请求的资源启用gzip
+        publicPath: conf.PUBLICBASE ? conf.PUBLICBASE + '/':'/', //设置打包文件访问地址
+        // historyApiFallback: true, //historyApi路由404后，转发到index.html
+        // stats: "errors-only",     //控制编译过程控制台输出
+        watchOptions: {              //有些文件系统下，需要手动开启轮询监视文件变化
+            poll: true
+        },
+        // setup(app) {},              //服务启动时执行中间件
+        before(app) {               //拦截请求
+            app.get('/some/path', function(req, res) {
+                res.json({ custom: 'response' });
+            });
+        },
+        // after(app) {},            //请求输出时处理
+        proxy: [                     //配置转发请求代理
             {
             context:[
                 `/**/*_v*.js`,
