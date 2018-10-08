29be72387c35c68252a998a3a04881e3bea04f51
fafer <zhoucheng502@sina.com>
2018-10-08 16:39:24 +0800

    update

--- build/conf.js
+++ build/conf.js
@@ -2,10 +2,8 @@ const path = require('path');
 const fs = require('fs');
 const resolveExtensions = /\.(jsx?|tsx?)$/;
 
-const getEntry = function (pathname,base,entry) {
-  if(!base) base = path.basename(pathname)
+const getEntry = function (pathname,base = path.basename(pathname),entry = {}) {
   let files = fs.readdirSync(pathname),name = '';
-  if(!entry) entry = {};
   files.forEach(function(file) {
       if(fs.lstatSync(path.join(pathname,file)).isDirectory()) {
           getEntry(path.join(pathname,file),`${base}/${file}`,entry);
@@ -17,14 +15,29 @@ const getEntry = function (pathname,base,entry) {
   return entry;
 }
 
+//项目根目录
 const ROOT_PATH = path.join(__dirname, '..');
+
+//打包输出目录
 const OUT_PATH = path.join(__dirname, '../dist');
+
+//打包入口目录
 const ENTRY_PATH = path.join(__dirname, '../src/pages');
+
+//copy目录
 const COPY_PATH = path.join(__dirname,'../src/lib');
+
+//copy目标目录
 const COPY_DEST_PATH = path.join(OUT_PATH,'lib');
+
+//解析entry
 const ENTRY = getEntry(ENTRY_PATH);
+
 const PUBLICBASE = '';
+//根目录base，默认未“/”
 const BASEPATH = PUBLICBASE ? PUBLICBASE + '/' : '/';
+
+//配置CDN,HOST
 const HOST = {
     js:'j1.58cdn.com.cn',
     css:'c.58cdn.com.cn',
