9f72ef0f659b3a2cf05e588dec1fbe238579e148
fafer <zhoucheng502@sina.com>
2018-10-10 19:31:00 +0800

    添加https代理

--- build/conf.js
+++ build/conf.js
@@ -1,18 +1,21 @@
 const path = require('path');
 const fs = require('fs');
+const del = require('del');
+const createCertificate = require('webpack-dev-server/lib/utils/createCertificate');
 const resolveExtensions = /\.(jsx?|tsx?)$/;
 
-const getEntry = function (pathname,base = path.basename(pathname),entry = {}) {
-  let files = fs.readdirSync(pathname),name = '';
-  files.forEach(function(file) {
-      if(fs.lstatSync(path.join(pathname,file)).isDirectory()) {
-          getEntry(path.join(pathname,file),`${base}/${file}`,entry);
-      } else if(resolveExtensions.test(file)){
-          name = `${base}/${file.replace(resolveExtensions,'')}`;
-          entry[name] = path.join(pathname,file);
-      }
-  });
-  return entry;
+const getEntry = function (pathname, base = path.basename(pathname), entry = {}) {
+    let files = fs.readdirSync(pathname),
+        name = '';
+    files.forEach(function (file) {
+        if (fs.lstatSync(path.join(pathname, file)).isDirectory()) {
+            getEntry(path.join(pathname, file), `${base}/${file}`, entry);
+        } else if (resolveExtensions.test(file)) {
+            name = `${base}/${file.replace(resolveExtensions,'')}`;
+            entry[name] = path.join(pathname, file);
+        }
+    });
+    return entry;
 }
 
 //项目根目录
@@ -25,10 +28,10 @@ const OUT_PATH = path.join(__dirname, '../dist');
 const ENTRY_PATH = path.join(__dirname, '../src/pages');
 
 //copy目录
-const COPY_PATH = path.join(__dirname,'../src/lib');
+const COPY_PATH = path.join(__dirname, '../src/lib');
 
 //copy目标目录
-const COPY_DEST_PATH = path.join(OUT_PATH,'lib');
+const COPY_DEST_PATH = path.join(OUT_PATH, 'lib');
 
 //解析entry
 const ENTRY = getEntry(ENTRY_PATH);
@@ -39,25 +42,76 @@ const BASEPATH = PUBLICBASE ? PUBLICBASE + '/' : '/';
 
 //配置CDN,HOST
 const HOST = {
-    js:'j1.58cdn.com.cn',
-    css:'c.58cdn.com.cn',
-    img:'img.58cdn.com.cn'
+    js: 'j1.58cdn.com.cn',
+    css: 'c.58cdn.com.cn',
+    img: 'img.58cdn.com.cn'
 };
-const PUBLICPATH = HOST.js?`//${HOST.js}${BASEPATH}`:BASEPATH;
-const IMGPUBLICPATH = HOST.img?`//${HOST.img}${BASEPATH}`:BASEPATH;
-const CSSPUBLICPATH = HOST.css?`//${HOST.css}${BASEPATH}`:BASEPATH;
+const PUBLICPATH = HOST.js ? `//${HOST.js}${BASEPATH}` : BASEPATH;
+const IMGPUBLICPATH = HOST.img ? `//${HOST.img}${BASEPATH}` : BASEPATH;
+const CSSPUBLICPATH = HOST.css ? `//${HOST.css}${BASEPATH}` : BASEPATH;
 
 module.exports = {
-  ROOT_PATH,
-  OUT_PATH,
-  ENTRY_PATH,
-  COPY_PATH,
-  COPY_DEST_PATH,
-  ENTRY,
-  HOST,
-  PUBLICBASE,
-  BASEPATH,
-  PUBLICPATH,
-  IMGPUBLICPATH,
-  CSSPUBLICPATH
+    ROOT_PATH,
+    OUT_PATH,
+    ENTRY_PATH,
+    COPY_PATH,
+    COPY_DEST_PATH,
+    ENTRY,
+    HOST,
+    PUBLICBASE,
+    BASEPATH,
+    PUBLICPATH,
+    IMGPUBLICPATH,
+    CSSPUBLICPATH,
+    ssl(options = {}) {
+        let fakeCert;
+        if (!options.key || !options.cert) {
+            // Use a self-signed certificate if no certificate was configured.
+            // Cycle certs every 24 hours
+            const certPath = path.join(ROOT_PATH, 'node_modules/webpack-dev-server/ssl/server.pem');
+
+            let certExists = fs.existsSync(certPath);
+
+            if (certExists) {
+                const certTtl = 1000 * 60 * 60 * 24;
+                const certStat = fs.statSync(certPath);
+
+                const now = new Date();
+
+                // cert is more than 30 days old, kill it with fire
+                if ((now - certStat.ctime) / certTtl > 30) {
+                    this.log.info('SSL Certificate is more than 30 days old. Removing.');
+
+                    del.sync([certPath], {
+                        force: true
+                    });
+
+                    certExists = false;
+                }
+            }
+
+            if (!certExists) {
+                this.log.info('Generating SSL Certificate');
+
+                const attrs = [{
+                    name: 'commonName',
+                    value: 'localhost'
+                }];
+
+                const pems = createCertificate(attrs);
+
+                fs.writeFileSync(
+                    certPath,
+                    pems.private + pems.cert, {
+                        encoding: 'utf-8'
+                    }
+                );
+            }
+            fakeCert = fs.readFileSync(certPath);
+        }
+        return {
+            key: options.key || fakeCert,
+            cert: options.cert || fakeCert
+        }
+    }
 }
--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -8,68 +8,72 @@ const argv = require('yargs').argv;
 
 let port;
 
-if(argv.https) {
-    if(os.platform() === 'darwin') port = 8042;
+if (argv.https) {
+    if (os.platform() === 'darwin') port = 8042;
     else port = 443;
 } else {
-    if(os.platform() === 'darwin') port = 8041;
+    if (os.platform() === 'darwin') port = 8041;
     else port = 80;
 }
 
 module.exports = Merge(CommonConfig, {
-    mode:'development',
+    mode: 'development',
     devtool: 'inline-source-map',
     devServer: {
-        host: '0.0.0.0',            //允许外网访问，默认值为localhost
+        host: '0.0.0.0', //允许外网访问，默认值为localhost
         port: port,
-        open:true,                  //服务启动后，打开浏览器默认访问：http://host:port/
-        openPage:'',                //设置默认打开的页面路径
-        public: "localhost",        //配置服务启动后，打开浏览器访问http://public,优先级最高>useLocalIp>默认
-        useLocalIp: true,           //配置服务启动后，打开浏览器访问:http://本地ip:port/,如果失败，转发到public设置
-        https: argv.https ? true :false,             //开启https
-        disableHostCheck: true,     //允许所有host域名访问
-        allowedHosts:[],            //设置允许访问的域名白名单
-        hot: true,                  //开启热更新
-        hotOnly: true,              //编译失败后，再次编译成功，不需要刷新页面更新
-        contentBase: [conf.ENTRY_PATH,path.join(conf.ROOT_PATH)], //配置访问静态资源根目录
-        compress: true,             //请求的资源启用gzip
-        publicPath: conf.BASEPATH,  //设置打包文件访问地址
+        open: true, //服务启动后，打开浏览器默认访问：http://host:port/
+        openPage: '', //设置默认打开的页面路径
+        public: "localhost", //配置服务启动后，打开浏览器访问http://public,优先级最高>useLocalIp>默认
+        useLocalIp: true, //配置服务启动后，打开浏览器访问:http://本地ip:port/,如果失败，转发到public设置
+        https: argv.https ? true : false, //开启https
+        disableHostCheck: true, //允许所有host域名访问
+        allowedHosts: [], //设置允许访问的域名白名单
+        hot: true, //开启热更新
+        hotOnly: true, //编译失败后，再次编译成功，不需要刷新页面更新
+        contentBase: [conf.ENTRY_PATH, path.join(conf.ROOT_PATH)], //配置访问静态资源根目录
+        compress: true, //请求的资源启用gzip
+        publicPath: conf.BASEPATH, //设置打包文件访问地址
         // historyApiFallback: true, //historyApi路由404后，转发到index.html
-        stats: "errors-only",     //控制编译过程控制台输出
-        watchOptions: {              //有些文件系统下，需要手动开启轮询监视文件变化
+        stats: "errors-only", //控制编译过程控制台输出
+        watchOptions: { //有些文件系统下，需要手动开启轮询监视文件变化
             poll: true
         },
         // setup(app) {},              //服务启动时执行中间件
         // before(app) {},              //拦截请求
         // after(app) {},            //请求输出时处理
-        proxy: [                     //配置转发请求代理
+        proxy: [ //配置转发请求代理
             {
-            context:[
-                `/**/*_v*.js`,
-                `/**/*_v*.`+'css'
-            ],
-            target: "//localhost",
-            changeOrigin: true,
-            pathRewrite: function (path, req) {
-                if(/\.js$/.test(path)) {
-                    return path.replace(/(_v.*\.js)$/i, '.js');
-                } else if(/\.css$/.test(path)) {
-                    return path.replace(/(_v.*\.css)$/i, '.css');
+                context: [
+                    `/**/*_v*.js`,
+                    `/**/*_v*.` + 'css'
+                ],
+                ssl: argv.https ? conf.ssl() : undefined,
+                secure: !!argv.https,
+                target: `${argv.https ? 'https':'http'}://127.0.0.1`,
+                changeOrigin: true,
+                pathRewrite: function (path, req) {
+                    console.log(path)
+                    if (/\.js$/.test(path)) {
+                        return path.replace(/(_v.*\.js)$/i, '.js');
+                    } else if (/\.css$/.test(path)) {
+                        return path.replace(/(_v.*\.css)$/i, '.css');
+                    }
                 }
+            }, {
+                context: [
+                    '/exchange/**',
+                    '/common/**',
+                    '/user/**',
+                    '/accountmanage/**',
+                    '/dashboard/**',
+                    '/special/**',
+                    '/wos/**'
+                ],
+                target: "http://sl.g.58.com",
+                changeOrigin: true,
             }
-        },{
-            context:[
-                '/exchange/**',
-                '/common/**',
-                '/user/**',
-                '/accountmanage/**',
-                '/dashboard/**',
-                '/special/**',
-                '/wos/**'
-            ],
-            target: "http://sl.g.58.com",
-            changeOrigin: true,
-        }]
+        ]
     },
     plugins: [
         new webpack.HotModuleReplacementPlugin()
