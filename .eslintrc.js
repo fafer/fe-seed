39237ac0919f702eb9de5028523035a5e2eb044e
fafer <zhoucheng502@sina.com>
2018-10-15 23:09:44 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,56 +1,38 @@
 module.exports = {
     "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true
+      "browser": true,
+      "commonjs": true,
+      "es6": true,
+      "node": true
     },
-    "extends": [
-        "eslint:recommended"
-    ],
+    "extends": "eslint:recommended",
+    "parser":"babel-eslint",
     "parserOptions": {
-        "ecmaFeatures": {
-            "experimentalObjectRestSpread": true,
-            "jsx": true
-        },
-        "sourceType": "module"
+      "ecmaFeatures": {
+          "jsx": true
+      },
+      "ecmaVersion": 2018,
+      "sourceType": "module"
     },
-    "plugins": ["react"],
-    "globals": {
-        "window": true,
-        "history": true,
-        "location": true,
-    },
-    "parser": "babel-eslint",
+    "plugins": [
+      "react"
+    ],
     "rules": {
-        "no-console": [
-            "error", {
-                "allow": ["warn", "error","log"]
-            }
-        ],
-        //禁止空格和 tab 的混合缩进
-        "no-mixed-spaces-and-tabs": [
-            "error", "smart-tabs"
-        ],
-        "indent": [
-            "error", 2
-        ],
-        "linebreak-style": 0,
-        "quotes": [
-            "error", "single"
-        ],
-        "semi": [
-            "error", "always"
-        ],
-        "react/jsx-filename-extension": ["warn"],
-        "react/jsx-uses-react": ["warn"],
-        "react/jsx-uses-vars": ["warn"],
-        "no-case-declarations": ["warn"],
-        "no-unused-vars": [
-            "warn", {
-                "vars": "all",
-                "args": "after-used",
-                "ignoreRestSiblings": false
-            }
-        ]
+      "indent": [
+        "error",
+        "tab"
+      ],
+      "linebreak-style": [
+        "error",
+        "unix"
+      ],
+      "quotes": [
+        "error",
+        "single"
+      ],
+      "semi": [
+        "error",
+        "always"
+      ]
     }
 };
