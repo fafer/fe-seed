708571eed3bcbe5eca8b26406dc6d76d12dcd462
fafer <zhoucheng502@sina.com>
2018-10-26 22:59:38 +0800

    updateconfig

--- build/https/createCertificate.js
+++ build/https/createCertificate.js
@@ -1,52 +1,53 @@
 'use strict';
 
 const selfsigned = require('selfsigned');
-const conf = require('../conf');
+const {HOST} = require('../conf');
 
-function createCertificate (attrs) {
+function createCertificate(attrs) {
 	return selfsigned.generate(attrs, {
 		algorithm: 'sha256',
 		days: 30,
 		keySize: 2048,
-		extensions: [
-			{
-				name: 'basicConstraints',
-				cA: true
+		extensions: [{
+			name: 'basicConstraints',
+			cA: true
+		},
+		{
+			name: 'keyUsage',
+			keyCertSign: true,
+			digitalSignature: true,
+			nonRepudiation: true,
+			keyEncipherment: true,
+			dataEncipherment: true
+		},
+		{
+			name: 'subjectAltName',
+			altNames: [{
+				// type 2 is DNS
+				type: 2,
+				value: 'localhost'
 			},
 			{
-				name: 'keyUsage',
-				keyCertSign: true,
-				digitalSignature: true,
-				nonRepudiation: true,
-				keyEncipherment: true,
-				dataEncipherment: true
+				type: 2,
+				value: '[::1]'
 			},
 			{
-				name: 'subjectAltName',
-				altNames: [
-					{
-						// type 2 is DNS
-						type: 2,
-						value: 'localhost'
-					},
-					{
-						type: 2,
-						value: '[::1]'
-					},
-					{
-						// type 7 is IP
-						type: 7,
-						ip: '127.0.0.1'
-					},
-					{
-						type: 7,
-						ip: 'fe80::1'
-					},
-					...Object.values(conf.HOST).map(d => {
-						return {type:2,value:d};
-					})
-				]
-			}
+				// type 7 is IP
+				type: 7,
+				ip: '127.0.0.1'
+			},
+			{
+				type: 7,
+				ip: 'fe80::1'
+			},
+			...Object.values(HOST).map(d => {
+				return {
+					type: 2,
+					value: d
+				};
+			})
+			]
+		}
 		]
 	});
 }
