cd20f642f6ab06aeabc7f419e309e96565f6a330
fafer <zhoucheng502@sina.com>
2019-01-08 15:57:56 +0800

    style:ftp

--- ftp.config.js
+++ ftp.config.js
@@ -2,19 +2,19 @@ const { OUT_PATH, PUBLICBASE } = require('./build/conf');
 const path = require('path');
 module.exports = [
   {
-    host: '192.168.185.128',
+    host: '10.252.185.206',
     port: 21,
-    user: 'qatest',
-    password: '58ftp@fe',
+    user: 'static',
+    password: 'fe@fafer',
     srcPath: OUT_PATH,
-    destPath: path.posix.join('/static.58.com/weitech/test', PUBLICBASE)
+    destPath: path.posix.join('/test', PUBLICBASE)
   },
   {
-    host: '192.168.178.15',
+    host: '10.252.185.206',
     port: 21,
-    user: 'qatest',
-    password: '58ftp@fe',
+    user: 'static',
+    password: 'fe@fafer',
     srcPath: OUT_PATH,
-    destPath: path.posix.join('/static.58.com/weitech/test', PUBLICBASE)
+    destPath: path.posix.join('/test', PUBLICBASE)
   }
 ];
