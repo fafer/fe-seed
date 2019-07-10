c23a840f1676d9bfd2688dc6f0b674c8bff795b3
fafer <zhoucheng502@sina.com>
2019-07-10 20:24:39 +0800

    refactor: ftp

--- ftp.config.js
+++ ftp.config.js
@@ -2,7 +2,7 @@ const { OUT_PATH, PUBLICBASE } = require('./build/conf');
 const path = require('path');
 module.exports = [
   {
-    host: '10.252.185.206',
+    host: '10.252.191.155',
     port: 21,
     user: 'static',
     password: 'fe@fafer',
@@ -10,7 +10,7 @@ module.exports = [
     destPath: path.posix.join('/test', PUBLICBASE)
   },
   {
-    host: '10.252.185.206',
+    host: '10.252.191.155',
     port: 21,
     user: 'static',
     password: 'fe@fafer',
