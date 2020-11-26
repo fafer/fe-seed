d8338aa7fe09ba45c3b47281edaa080baacb6592
fafer <zhoucheng502@sina.com>
2020-11-26 19:57:03 +0800

    build: mock loader sourcemap

--- build/loaders/mock-loader/index.js
+++ build/loaders/mock-loader/index.js
@@ -1,11 +1,11 @@
 const { getOptions } = require('loader-utils');
 const validateOptions = require('schema-utils');
 
-module.exports = function (source) {
+module.exports = function (source, sourceMap) {
   const options = getOptions(this) || {};
   validateOptions(require('./options.json'), options, 'Mock Loader');
   if (options.enable) {
     source = source.replace(/\/\/\s*@mock/g, '');
   }
-  return source;
+  this.callback(null, source, sourceMap);
 };
